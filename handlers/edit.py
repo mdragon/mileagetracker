109acf01abd2b09ab2ccb391d603dd37d8477248
Matt Dragon <mdragon@gmail.com>
2013-08-01 08:56:37 -0400

    Style fixes from various PEPs suggested by Sublime Text

--- handlers/edit.py
+++ handlers/edit.py
@@ -9,18 +9,19 @@ from google.appengine.ext.webapp import template
 
 import models
 
+
 class Edit(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
         key = self.request.get('key')
-        if user == None:
+        if user is None:
             self.redirect(self.request.relative_url(users.create_login_url("/Edit?key=" + key)))
             return
-           
-        if users.is_current_user_admin() == False:
+
+        if not users.is_current_user_admin():
             self.redirect("/?not-admin")
             return
-    
+
         if key != "":
             entry = db.get(key)
         else:
@@ -31,7 +32,7 @@ class Edit(webapp2.RequestHandler):
             entry.prettyDate = entry.date.strftime('%m/%d/%y')
         else:
             key = ""
-    
+
         template_values = {
             'entry': entry,
             'key': key,
@@ -40,26 +41,26 @@ class Edit(webapp2.RequestHandler):
             'logout_link': users.create_logout_url('/'),
             'user': users.get_current_user()
         }
-        
+
         path = os.path.join(os.path.dirname(__file__), '../views/edit.html')
         #self.response.out.write(path)
-        self.response.out.write(template.render(path, template_values))	
-    
+        self.response.out.write(template.render(path, template_values))
+
     def post(self):
-        if users.is_current_user_admin() == False:
+        if not users.is_current_user_admin():
             self.redirect("/?not-admin")
             return
-        
+
         key = self.request.get('key')
         if key != "":
             entry = db.get(key)
         else:
             entry = models.Entry()
-        
-        if( len(self.request.get('date').strip().split(' ')) > 1 ):
-            entry.date =  datetime.datetime.strptime(self.request.get('date'), '%Y-%m-%d %H:%M:%S')
+
+        if(len(self.request.get('date').strip().split(' ')) > 1):
+            entry.date = datetime.datetime.strptime(self.request.get('date'), '%Y-%m-%d %H:%M:%S')
         else:
-            entry.date =  datetime.datetime.strptime(self.request.get('date'), '%m/%d/%y')
+            entry.date = datetime.datetime.strptime(self.request.get('date'), '%m/%d/%y')
         entry.gallons = float(self.request.get('gallons'))
         entry.cost = float(self.request.get('cost'))
         entry.miles = int(self.request.get('miles'))
@@ -77,57 +78,60 @@ class Edit(webapp2.RequestHandler):
         #path = os.path.join(os.path.dirname(__file__), '../views/edit.html')
         #self.response.out.write(path)
         #self.response.out.write(template.render(path, template_values))
-        
+
+
 class Delete(webapp2.RequestHandler):
     def get(self):
-        if users.is_current_user_admin() == False:
+        if not users.is_current_user_admin():
             self.redirect("/")
             return
-        
+
         key = self.request.get('key')
         if key != "":
             entry = db.get(key)
 
-        if entry != None and entry.is_saved():
+        if not entry is None and entry.is_saved():
             db.delete(key)
-            
+
         self.redirect("/?delete")
-    
+
+
 class Migration1(webapp2.RequestHandler):
     def get(self):
         if len(models.Car().all().fetch(999)) > 0:
             logging.debug('skipping migration there are already Cars in the datastore')
             return
-        
+
         c = models.Car()
         c.name = '09 Civic'
         c.make = 'Honda'
         c.model = 'Civic'
         c.year = '2009'
-        c.leaseStart = datetime.datetime.strptime('2009-08-01','%Y-%m-%d').date()
-        c.leaseEnd = datetime.datetime.strptime('2012-06-29','%Y-%m-%d').date()
-        
+        c.leaseStart = datetime.datetime.strptime('2009-08-01', '%Y-%m-%d').date()
+        c.leaseEnd = datetime.datetime.strptime('2012-06-29', '%Y-%m-%d').date()
+
         c.put()
-        
+
         entries = models.Entry.all()
         es = entries.fetch(999)
         logging.debug('setting car on ' + str(len(es)) + ' entities')
         for e in es:
             e.car = c
             e.put()
-        
+
         c = models.Car()
         c.name = '12 Accord'
         c.make = 'Honda'
         c.model = 'Accord'
         c.year = '2012'
         c.startingMiles = 105
-        c.leaseStart = datetime.datetime.strptime('2012-06-29','%Y-%m-%d').date()
-        c.leaseEnd = datetime.datetime.strptime('2015-06-29','%Y-%m-%d').date()
+        c.leaseStart = datetime.datetime.strptime('2012-06-29', '%Y-%m-%d').date()
+        c.leaseEnd = datetime.datetime.strptime('2015-06-29', '%Y-%m-%d').date()
         c.default = True
-        
+
         c.put()
-        
+
+
 editApp = webapp2.WSGIApplication(
                                      [('/edit/delete', Delete),
                                      ('/edit/migration1', Migration1),
--- handlers/view.py
+++ handlers/view.py
@@ -9,135 +9,140 @@ from google.appengine.ext.webapp import template
 
 import models
 
+
 class Main(webapp2.RequestHandler):
-  def get(self):
-    if 'car' in self.request.arguments():
-        cars = models.Car.get(self.request.get('car'))
-    else:
-        cars = models.Car.all().filter('default = ', True).fetch(1)
-        if len(cars) > 0:
-            car = cars[0]
+    def get(self):
+        if 'car' in self.request.arguments():
+            cars = models.Car.get(self.request.get('car'))
         else:
-            car = models.Car()
-            car.name = "Placeholder"
-            car.make = "My"
-            car.model = "Car"
-            car.year = "2013"
-            car.default = True
-            car.leaseStart = datetime.date(2013, 01, 01)
-            car.leaseEnd = datetime.date(2016, 01, 01)
-            car.startingMiles = 0
-            car.put()
-    entries = self.loadEntries(car)
-
-    last = None
-    anychange = False
-    calcMPG = 0.0
-    avgMPG = 0.0
-    avgCPG = 0.0
-    mpgs = []
-    cpgs = []
-    totalGallons = 0.0
-    totalMPG = 0.0
-    miles = [
-        {'date': car.leaseEnd, 'miles': None, 'lease': 36000, 'jstime': 0}
-    ]
-    for e in entries:
-        change = False
-        miles.append({'date': e.date.date(), 'miles': e.miles, 'lease': getLease(e.date.date(), car.leaseStart), 'jstime': 0})
-        
-        if e.cpg is None:
-            e.cpg = e.calc_cpg()
-            change = True
-
-        if last is not None:
-            calcMPG = e.calc_mpg(last)
-            if e.mpg != calcMPG:
-                e.mpg = calcMPG
-                change = True
-        else:
-            if car.startingMiles > 0:
-                logging.debug('using car ' + car.name + ' starting miles for first mpg ' + str(car.startingMiles) + ' entry ' + str(e.date.date()))
-                e.mpg = e.calc_mpg_miles(car.startingMiles)
+            cars = models.Car.all().filter('default = ', True).fetch(1)
+            if len(cars) > 0:
+                car = cars[0]
             else:
-                if e.mpg != 0.0:
-                    e.mpg = 0.0
-            change = True
-        
-        if change:
-            anychange = True
-            db.put(e)
-
-        if e.mpg != 0:
-            avgMPG += e.mpg
-            totalMPG += 1
-
-        avgCPG += (e.cpg * e.gallons)
-        mpgs.append(e.mpg)
-        cpgs.append(e.cpg)
-        totalGallons += e.gallons
-
-        last = e
-
-    if anychange:
+                car = models.Car()
+                car.name = "Placeholder"
+                car.make = "My"
+                car.model = "Car"
+                car.year = "2013"
+                car.default = True
+                car.leaseStart = datetime.date(2013, 01, 01)
+                car.leaseEnd = datetime.date(2016, 01, 01)
+                car.startingMiles = 0
+                car.put()
         entries = self.loadEntries(car)
 
-    mpgs.sort()
-    cpgs.sort()
+        last = None
+        anychange = False
+        calcMPG = 0.0
+        avgMPG = 0.0
+        avgCPG = 0.0
+        mpgs = []
+        cpgs = []
+        totalGallons = 0.0
+        totalMPG = 0.0
+        miles = [
+            {'date': car.leaseEnd, 'miles': None, 'lease': 36000, 'jstime': 0}
+        ]
+        for e in entries:
+            change = False
+            miles.append({'date': e.date.date(), 'miles': e.miles, 'lease': getLease(e.date.date(), car.leaseStart), 'jstime': 0})
 
-    medMPG = median(mpgs)
-    medCPG = median(cpgs)
+            if e.cpg is None:
+                e.cpg = e.calc_cpg()
+                change = True
 
-    if totalMPG == 0:
-        avgMPG = 0
-        avgCPG = 0
-    else:
-        avgMPG = avgMPG/totalMPG
-        avgCPG = avgCPG/totalGallons
+            if last is not None:
+                calcMPG = e.calc_mpg(last)
+                if e.mpg != calcMPG:
+                    e.mpg = calcMPG
+                    change = True
+            else:
+                if car.startingMiles > 0:
+                    logging.debug('using car ' + car.name + ' starting miles for first mpg ' + str(car.startingMiles) + ' entry ' + str(e.date.date()))
+                    e.mpg = e.calc_mpg_miles(car.startingMiles)
+                else:
+                    if e.mpg != 0.0:
+                        e.mpg = 0.0
+                change = True
 
-    for m in miles:
-        # for k in m.keys():
-        #     if m[k] is None:
-        #         logging.debug('m ' + k)
-        #     else:
-        #         logging.debug('m ' + k + ' = ' + str(m[k]))
-        if m['date'] is not None:
-            m['jstime'] = jstime(m['date'])
+            if change:
+                anychange = True
+                db.put(e)
+
+            if e.mpg != 0:
+                avgMPG += e.mpg
+                totalMPG += 1
+
+            avgCPG += (e.cpg * e.gallons)
+            mpgs.append(e.mpg)
+            cpgs.append(e.cpg)
+            totalGallons += e.gallons
+
+            last = e
+
+        if anychange:
+            entries = self.loadEntries(car)
+
+        mpgs.sort()
+        cpgs.sort()
+
+        medMPG = median(mpgs)
+        medCPG = median(cpgs)
+
+        if totalMPG == 0:
+            avgMPG = 0
+            avgCPG = 0
         else:
-            m['jstime'] = 0
+            avgMPG = avgMPG/totalMPG
+            avgCPG = avgCPG/totalGallons
 
-    miles.sort()
+        for m in miles:
+            # for k in m.keys():
+            #     if m[k] is None:
+            #         logging.debug('m ' + k)
+            #     else:
+            #         logging.debug('m ' + k + ' = ' + str(m[k]))
+            if m['date'] is not None:
+                m['jstime'] = jstime(m['date'])
+            else:
+                m['jstime'] = 0
 
-    template_values = {
-      'entries': entries,
-      'avgMPG': avgMPG,
-      'medMPG': medMPG,
-      'avgCPG': avgCPG,
-      'medCPG': medCPG,
-      'admin': users.is_current_user_admin(),
-      'login_link': users.create_login_url(self.request.url),
-      'logout_link': users.create_logout_url('/'),
-      'user': users.get_current_user(),
-      'miles': miles,
-      'car': car
-      }
+        miles.sort()
 
-    path = os.path.join(os.path.dirname(__file__), '../views/main.html')
-    self.response.out.write(template.render(path, template_values))
+        template_values = {
+            'entries': entries,
+            'avgMPG': avgMPG,
+            'medMPG': medMPG,
+            'avgCPG': avgCPG,
+            'medCPG': medCPG,
+            'admin': users.is_current_user_admin(),
+            'login_link': users.create_login_url(self.request.url),
+            'logout_link': users.create_logout_url('/'),
+            'user': users.get_current_user(),
+            'miles': miles,
+            'car': car
+        }
 
-  def loadEntries(self, car):
+        path = os.path.join(os.path.dirname(__file__), '../views/main.html')
+        self.response.out.write(template.render(path, template_values))
+
+
+def loadEntries(self, car):
     entries = models.Entry.all().filter('car = ', car)
     entries.order("date")
-    
+
     return entries
 
+
 def jstime(date):
-    d3 = date - datetime.datetime(1970,1,1).date()
+    d3 = date - datetime.datetime(1970, 1, 1).date()
     return ((d3.days * 86400000000) + (d3.seconds + 1000000) + d3.microseconds)/1000
+
 
 def getLease(date, start):
     d3 = date - start
-    return 36000/(3*365)* d3.days
+    return 36000/(3*365) * d3.days
+
 
 def median(data):
     dlen = len(data)
@@ -149,9 +154,9 @@ def median(data):
             val = sum/2
         else:
             val = data[dlen/2]
-    
+
     return val
 
 mainApp = webapp2.WSGIApplication(
-                                     [('/', Main)],
-                                     debug=True)
+    [('/', Main)],
+    debug=True)
